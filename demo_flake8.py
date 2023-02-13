6f7fa2b77330b385606349a0e970d5750492a69b
Vincent <v.villette976@gmail.com>
2023-02-13 15:26:34 +0100

    python ci modif utf 8 v5

--- demo_flake8.py
+++ demo_flake8.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 print("hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world")
 package_dir = Path(__file__).parent.absolute()
 print(package_dir)
